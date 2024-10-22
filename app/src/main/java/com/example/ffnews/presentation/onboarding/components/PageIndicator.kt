c831ed67d0462dbd1130e30f0aa9dd4f2fc72b4c
Fatih Çetiner <f-cetiner99@hotmail.com>
2024-10-22 22:09:48 +0300

    OnBoarding screen design completed

---
+++ app/src/main/java/com/example/ffnews/presentation/onboarding/components/PageIndicator.kt
@@ -0,0 +1,39 @@
+package com.example.ffnews.presentation.onboarding.components
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.example.ffnews.presentation.Dimens.IndicatorSize
+
+@Composable
+fun PageIndicator(
+    modifier: Modifier = Modifier,
+    pagesSize: Int,
+    selectedPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.secondaryContainer,
+    unselectedColor: Color = MaterialTheme.colorScheme.outline
+) {
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        repeat(pagesSize) { page ->
+            Box(
+                modifier = Modifier
+                    .size(IndicatorSize)
+                    .clip(CircleShape)
+                    .background(color = if (page == selectedPage) selectedColor else unselectedColor)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/ffnews/ui/MainActivity.kt
+++ app/src/main/java/com/example/ffnews/ui/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.ffnews.ui.theme.MyappTheme
 import androidx.compose.material3.Text
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.example.ffnews.presentation.onboarding.OnBoardingScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             MyappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(text = "Hello, World!", modifier = Modifier.padding(innerPadding))
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
                 }
             }
         }
